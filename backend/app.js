596256c73806f6c7a11d94ebfd78ab9f1d8a0890
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-13 16:10:57 +0100

    Mise en place de mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 //Commande pour importer express
 const express = require('express');
+const mongoose = require('mongoose');
 //Appel de la méthode express, qui permet de créer l'application express
 const app = express();
+
+mongoose.connect('mongodb+srv://abwb:projet6oc@cluster0.wzvyrvy.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 //Gestion de la fonction middleware
 app.use((req, res)=>{
     res.json({message: 'Voila la réponse de ma requête'});
