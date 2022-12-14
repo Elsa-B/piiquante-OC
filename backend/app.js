0aa8a5322ae5ba298d6fda2109b77aa1e6ec4f04
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-14 12:37:52 +0100

    authentification

--- backend/app.js
+++ backend/app.js
@@ -4,18 +4,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 //Commande d'import de user depuis le dossier routes
 const userRoutes = require('./routes/user');
-//Appel de la méthode express, qui permet de créer l'application express
-const app = express();
+//Intercepte toutes les requêtes
+//app.use(express.json())
 
 mongoose.connect('mongodb+srv://abwb:projet6oc@cluster0.wzvyrvy.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-//Gestion de la fonction middleware
-app.use((req, res)=>{
-    res.json({message: 'Voila la réponse de ma requête'});
+
+//Appel de la méthode express, qui permet de créer l'application express
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
+
+app.use((req, res) => {
+   res.json({ message: 'Votre requête a bien été reçue !' }); 
+});
+
 app.use('/api/auth', userRoutes)
 //Export de la méthode express
 module.exports = app;
