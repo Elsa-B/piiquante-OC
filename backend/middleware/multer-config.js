f0555be06c7f14f92ad79a6c85c5abfbd8e52e14
Elsa-B <elsaz2.barrero@gmail.com>
2022-12-20 12:37:52 +0100

    Finalisation multer

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,20 @@
-const multer = require('multer');
-
+const multer = require('multer');//Import de multer
+//Objet avec les différents types d'images
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
-
-const storage = multer.diskStorage({
+//Création d'un objet de configuration 
+const storage = multer.diskStorage({//Enregistrement sur le disque
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, 'images');//Pas d'erreur, nom du dossier
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    const name = file.originalname.split(' ').join('_');//On génère le nom du fichier.On supprime les espaces et instaure des underscores dans le nom de fichier
+    const extension = MIME_TYPES[file.mimetype];//Création de l'extension de fichier 
+    callback(null, name + Date.now() + '.' + extension);//On appel le callback avec le nom de fichier
   }
 });
-
+//On exporte multer avec l'objet storage et le fichier unique
 module.exports = multer({storage: storage}).single('image');
